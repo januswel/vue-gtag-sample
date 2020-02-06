4623a068b38685f44376c2e25c44b3ac8632f89b
Takagi Kensuke <janus.wel.3@gmail.com>
2020-02-06 14:25:54 +0900

    :sparkles::heavy_plus_sign: introduece vue-gtag

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import VueGtag from "vue-gtag";
 
 import router from "./Routes";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+Vue.use(VueGtag, {
+  config: {
+    id: "UA-157906734-1"
+  }
+});
 
 new Vue({
   el: "#app",
