63a7de25f6aa9b712766fa844bf072c6ae1b6e14
Takagi Kensuke <janus.wel.3@gmail.com>
2020-02-06 13:10:54 +0900

    :wrench::sparkles: show lifecycles

---
+++ src/components/LifeCycles.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>foo</div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy");
+  },
+  destroyed() {
+    console.log("destroyed");
+  }
+};
+</script>
